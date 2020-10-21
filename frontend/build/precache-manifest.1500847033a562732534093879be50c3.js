fdd18b37204c1a6e31cd172cae9771340021566b
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2020-10-21 16:53:04 -0700

    added build to git

---
+++ frontend/build/precache-manifest.1500847033a562732534093879be50c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625277f61beabeed5f2483c2546acaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceae94df44af7cf39569",
+    "url": "/static/css/main.f795f613.chunk.css"
+  },
+  {
+    "revision": "a786a224662132d80879",
+    "url": "/static/js/2.9cf7e567.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.9cf7e567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceae94df44af7cf39569",
+    "url": "/static/js/main.3d1d4fdf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2dcd22a26f252de9bed061330409d774",
+    "url": "/static/media/bullpen.2dcd22a2.jpg"
+  },
+  {
+    "revision": "0abf2d168863080043c30c992c8f1627",
+    "url": "/static/media/dirt.0abf2d16.jpg"
+  },
+  {
+    "revision": "50bdde0e994135404bee92064b1fa896",
+    "url": "/static/media/flames.50bdde0e.jpg"
+  },
+  {
+    "revision": "ec8a6f5d15c0220c20fc313baee38cd2",
+    "url": "/static/media/magnifying.ec8a6f5d.jpg"
+  },
+  {
+    "revision": "3b7ff6f3d9b7bae4444cf83c7a555e9a",
+    "url": "/static/media/rating.3b7ff6f3.jpg"
+  },
+  {
+    "revision": "2ecb801f7779d94299b66b6ce25ea94c",
+    "url": "/static/media/spotify.2ecb801f.jpg"
+  }
+]);
