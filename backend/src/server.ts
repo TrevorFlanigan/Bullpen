9f1ad7a1b2f21636ae47231b97ecd481f387ae05
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2020-10-23 16:05:31 -0700

    deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(path.resolve(__dirname, "..", "..", "public")));
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "..", "public", "index.html"));
   });
 }
