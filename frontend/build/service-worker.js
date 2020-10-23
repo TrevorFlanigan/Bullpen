de645563a0cb60a7bb105b8f6f5a79b75bde3a14
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2020-10-23 15:26:27 -0700

    deploy

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddb29436836d8e7285a804561052bd3c.js"
+  "/precache-manifest.4d34c46255cbfefe38b118ce0d88fb52.js"
 );
 
 self.addEventListener('message', (event) => {
