30a4530f5ae9ea6d985d7b7e3ac7b41bc4c8c7f9
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2021-01-20 13:47:52 -0800

    frontend build

---
+++ backend/public/precache-manifest.e5a0320f04de5bf85fe44a0da20078d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ab88067033cd8616a2cb0d12733e0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4152ba9d03e53f7e2051",
+    "url": "/static/css/main.4f8c1220.chunk.css"
+  },
+  {
+    "revision": "9c17833e8ae5a345f0c4",
+    "url": "/static/js/2.2c8005b1.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.2c8005b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4152ba9d03e53f7e2051",
+    "url": "/static/js/main.78669235.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "a27c6a65ae308743158967824fcc023c",
+    "url": "/static/media/bullpen.a27c6a65.jpg"
+  },
+  {
+    "revision": "49044d85ff5c5668264a717f6e6b6db2",
+    "url": "/static/media/dirt.49044d85.jpg"
+  },
+  {
+    "revision": "83672b379b33dcb5692a6d69acf7b9f5",
+    "url": "/static/media/flames.83672b37.jpg"
+  },
+  {
+    "revision": "5bb933750ae160e45df8a84bbe58cbfa",
+    "url": "/static/media/magnifying.5bb93375.jpg"
+  },
+  {
+    "revision": "809a6c69864f7eef90042e62cceb4dc8",
+    "url": "/static/media/rating.809a6c69.jpg"
+  },
+  {
+    "revision": "33fcf95a45e35aa4db012953080285f2",
+    "url": "/static/media/spotify.33fcf95a.jpg"
+  }
+]);
